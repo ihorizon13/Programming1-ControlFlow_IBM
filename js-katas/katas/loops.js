7a8bc13c5ab8d946e6fb684d4db288d85e0fa2b9
Isisel Badillo-Matiaz <ibadillo-matiaz@icstars.org>
2026-02-22 20:22:55 -0600

    correct answer, provided by Jess on Friday for loops.js kata

--- js-katas/katas/loops.js
+++ js-katas/katas/loops.js
@@ -5,21 +5,20 @@ function firstNEven(n = 10) {
   // Return an array of the first n even numbers (starting at 2).
   // Example: firstNEven(5) -> [2, 4, 6, 8, 10]
   // Explain your logic choices in comments as you code.
-  if(n < 1){
-  throw new Error('Not implemented');
+  if(n < 0){
+  throw new Error('Not implemented, n must be non implemented');
 }
 const evenNumbersResults = []; //setting array as "even..."
 
-for(let i = 2; i <= n; i += 1){ //count from 2 to 10, stop at 10
- if( i % 2 === 0){ //divisible by 2, leaves no remainder
-    evenNumbersResults.push(i); //prints out for loop results
+for(let i = 1; i <= n; i += 1){ //count from 2 to 10, stop at 10
+ //divisible by 2, leaves no remainder
+    evenNumbersResults.push(i * 2); //prints out i and multiplies it by 2
  
-} 
+ 
 }
 return evenNumbersResults;
 }
 
-firstNEven(5);
 
 globalThis.__katas = globalThis.__katas || {};
 globalThis.__katas.firstNEven = firstNEven;
